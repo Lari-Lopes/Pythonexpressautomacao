44e5e9ada41a9ff00da62486996b67e3be66d38c
Lari <laradev22@gmail.com>
2024-07-07 18:02:08 -0300

    atualizao

--- automacao.py
+++ automacao.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pyautogui
 import time
-
 #Iniciar a automação
 pyautogui.PAUSE = 0.5
 
@@ -9,12 +8,48 @@ pyautogui.PAUSE = 0.5
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
-time.sleep(4)
+time.sleep(2)
 
 pyautogui.write("http://www.sauer.pro.br/python/automacao/index.html")
 pyautogui.press("enter")
-pyautogui.click(x=-1282, y=917)
+
+pyautogui.click(x=802, y=426)
 pyautogui.write("admin")
 
-#pyautogui.press("enter")
-#pyautogui.write("admin")
+pyautogui.press("tab")
+pyautogui.write("admin")
+
+
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+time.sleep(3)
+pyautogui.click(x=764, y=355)
+
+tabela = pd.read_csv("alunos.csv")
+
+for linha in tabela.index:
+    pyautogui.click(x=764, y=355)
+    nome = tabela.loc[linha, "Nome"]
+
+    pyautogui.write(str(nome))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha, "Email"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "Endereco"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "Telefone"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    time.sleep(3)
+    pyautogui.scroll(5000)
+
+
+
+
+    
+
+
+
+
